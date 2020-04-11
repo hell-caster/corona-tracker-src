c506800607c9747d2d852ac6f04da3a3d6bdaf6b
Jalaj <jalajdorai100@gmail.com>
2020-04-11 10:02:56 +0530

    corona-tracker

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+// import Cards from './components/Cards'
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+import coronaImage from './image/image.png';
+
+class App extends Component {
+  state = {  
+      data: {},
+      country:'',
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData});
+    // console.log(data);
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    
+    //console.log(fetchedData);
+    this.setState({ data: fetchedData,country: country});
+  }
+
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="COVID-19" />
+        <Cards data={data}/>
+        <CountryPicker handleCountryChange ={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
